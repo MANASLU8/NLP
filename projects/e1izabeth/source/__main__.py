4112e275439d36b4ff7d9d114ce52f3dbd2e32c5
E1izabeth <kuzenkova.el@yandex.ru>
2022-02-18 12:51:01 +0300

    Add more patterns for tokenizer and use pos tag for lemmatizetion

--- projects/e1izabeth/source/__main__.py
+++ projects/e1izabeth/source/__main__.py
@@ -1,13 +1,26 @@
 import os
-from pathlib import Path
 
+import nltk
 import pandas as pd
+from pathlib import Path
 from nltk import SnowballStemmer, WordNetLemmatizer
-
 from tokenizer.tokenizer import tokenize_text, classes
+from nltk.corpus import wordnet
 
 stemmer = SnowballStemmer("english")
 lemmatizer = WordNetLemmatizer()
+
+endOfClause = ['.', '?', '!']
+
+
+def get_wordnet_pos(word):
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+    return tag_dict.get(tag, wordnet.NOUN)
+
 
 def process_file(fname):
     print('working on ', fname)
@@ -26,23 +39,31 @@ def process_file(fname):
             for i in range(1, len(row)):
                 text = row[i]
                 tokens = tokenize_text(text)
+
+                prev = [0, '', '']
+                current_clause = None
                 for w in tokens:
-                    f.write(w[2] + (('\t' + stemmer.stem(w[2]) + "\t" + lemmatizer.lemmatize(w[2])) if w[1] == 'word' else "") + '\n')
+                    if w[1] != 'whitespace':
+                        f.write(w[2] + '\t' + stemmer.stem(w[2]) + "\t" + lemmatizer.lemmatize(w[2], get_wordnet_pos(w[2])) + '\n')
+                    elif prev[2] in endOfClause:
+                        f.write('\n')
+                    prev = w
+                f.write('\n')
             f.close()
         except Exception as e:
             print(e)
             print([n, text, tokens])
             pass
         n = n + 1
-        if n%1000==0:
-            print(int(n*100/dataCount), '%')
+        if n % 1000 == 0:
+            print(int(n * 100 / dataCount), '%')
 
 def main():
-    print(lemmatizer.lemmatize("companies"))
     fname_train = '../assets/raw-dataset/train.csv'
     fname_test = '../assets/raw-dataset/test.csv'
     process_file(fname_train)
     process_file(fname_test)
+
 
 if __name__ == "__main__":
     main()
--- projects/e1izabeth/source/tokenizer/tokenizer.py
+++ projects/e1izabeth/source/tokenizer/tokenizer.py
@@ -1,16 +1,33 @@
 from xml.sax import saxutils
 import re
 
+knownAbbrevs = {
+    "st.": "saint",
+    "dr.": "doctor",
+    "tel.": "telephone",
+    "no.": "number",
+    "u.s.": "United States",
+    "prof.": "professor",
+    "inc.": "incorporated",
+    "ltd.": "limited",
+    "corp.": "corporation",
+    "mr.": "mister"
+}
 
 tokens = [
+    ["abbrev", "|".join(map(lambda kv: "(?i:" + re.escape(kv[0]) + ")", knownAbbrevs.items()))],
+    ["ipaddress", "[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+"],
+    ["numeral", "[0-9]+((th)|(\\'s))"],
+    ["metrics", "[0-9]+(-)?[A-Z]?[a-z]+"],
+    ["currency", "\\$[0-9]+(\\,[0-9]+)*"],
     ["number", "[0-9]+(.|,|-)[0-9]*"],
     ["whitespace", "\\s|\\n|\\\\|\\t"],
-    ["punct", ",|\\.|\\?|\\!|(\\.\\.\\.)|\\(|\\)"],
+    ["braces", "\\(|\\)"],
     ["quoted", "(\\\")[^\\\"]*(\\\")"],
+    ["url", "[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"],
+    ["punct", ",|\\.|\\?|\\!|(\\.\\.\\.)"],
     ["word", "[A-Za-z][A-Za-z\\']*(-[A-Z\\']?[A-Za-z\\']+)*"],
-    #["abbrev", "[A-Z]{2,}"],
-    ["metrics", "[0-9]+(-)?[A-Z]?[a-z]+"],
-    ["other", ".[^a-zA-Z0-9]*"],
+    ["other", ".[^a-zA-Z0-9]*"]
 ]
 
 regex = re.compile("^(" + "|".join(map(lambda t: "(?P<" + t[0] + ">" + t[1] + ")", tokens)) + ")")
@@ -23,7 +40,6 @@ classes = dict([
 ])
 
 def tokenize_text(text):
-    data = saxutils.unescape(text)
     pos = 0
     s = text
     line = []
@@ -33,7 +49,12 @@ def tokenize_text(text):
             for gr in tokens:
                 tt = list(filter(lambda kv: kv[1] is not None, match.groupdict().items()))
                 if len(tt) == 1:
-                    line.append([pos, tt[0][0], tt[0][1]])
+                    kind = tt[0][0]
+                    part = tt[0][1]
+                    if kind == 'abbrev':
+                        kind = 'word'
+                        part = knownAbbrevs[part.lower()]
+                    line.append([pos, kind, part])
                     pos += len(tt[0][1])
                     s = s[len(tt[0][1]):]
                     break;
@@ -42,3 +63,5 @@ def tokenize_text(text):
         else:
             print('failed to tokenize: ' + s)
     return line
+
+
