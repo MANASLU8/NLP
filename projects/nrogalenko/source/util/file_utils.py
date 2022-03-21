9b7f7a858b738794bf03b75bec6edd42a1aa72ea
Nikita Rogalenko <nikitalr@mail.ru>
2022-03-21 19:27:08 +0300

    lab 4

--- projects/nrogalenko/source/util/file_utils.py
+++ projects/nrogalenko/source/util/file_utils.py
@@ -30,6 +30,20 @@ def get_tokens_list_with_token_tags_from_document_annotation(document_path):
     return tokens
 
 
+def get_lemmas_list_with_token_tags_from_document_annotation(document_path):
+    tokens = []
+    with open(document_path, "r") as f:
+        line = f.readline()
+        while line:
+            if line:
+                token = line.split("    ")[0]
+                if token != '\n':
+                    tokens.append((line.split("    ")[2], line.split("    ")[4][:-1]))
+            line = f.readline()
+    f.close()
+    return tokens
+
+
 def get_document_annotation_tokens_count(path):
     counter = 0
     with open(path, "r") as f:
