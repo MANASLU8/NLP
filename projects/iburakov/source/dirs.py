e6a144a8b3c6d9bf2acd53ce29b8d9c94dff3d15
iburakov <speedwatson@gmail.com>
2022-04-01 16:02:05 +0300

    Fix case of lda_experiments_dir

--- projects/iburakov/source/dirs.py
+++ projects/iburakov/source/dirs.py
@@ -7,7 +7,7 @@ dataset_dir = assets_dir / "raw-dataset"
 annotated_corpus_dir = assets_dir / "annotated-corpus"
 corrupted_dataset_dir = assets_dir / "corrupted-dataset"
 misc_dir = assets_dir / "misc"
-lda_experiments_dir = assets_dir / "lda_experiments"
+lda_experiments_dir = assets_dir / "lda-experiments"
 
 token_dictionary_filepath = annotated_corpus_dir / "tokens.tsv"
 term_document_matrix_filepath = annotated_corpus_dir / "term-document-matrix.npz"
