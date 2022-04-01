a506bb85d02280074c27bd6074ea5bbce421683b
Naymoll <xdarkknight122x@gmail.com>
2022-04-01 11:50:00 +0300

    Refactor 1 lab

--- projects/msokolov/source/tokenizer/__main__.py
+++ projects/msokolov/source/tokenizer/__main__.py
@@ -1,17 +1,13 @@
-import re
 import sys
 import csv
 import nltk
+import itertools
 
 from nltk import SnowballStemmer, WordNetLemmatizer
 from pathlib import Path
 
 from source.tokenizer import Tokenizer
-from record import Sentence, Record
-from patterns import sentence_pattern
-
-sentence_regex = re.compile(sentence_pattern)
-tokenizer = Tokenizer()
+from record import Record, TokenizedRecord
 
 
 def init_ntlk():
@@ -21,28 +17,28 @@ def init_ntlk():
     nltk.download('universal_tagset')
 
 
-def split_to_sentences(text: str):
-    return list(map(lambda s: Sentence(tokenizer.tokenize(s)), sentence_pattern.split(text)))
-
-
 def read_records(path: str):
     records = []
     with open(path) as file:
         reader = csv.reader(file)
         for [label, title, text] in reader:
-            sentences = []
-            title_sentences = split_to_sentences(title)
-            sentences.extend(title_sentences)
-
-            text_sentences = split_to_sentences(text)
-            sentences.extend(text_sentences)
-
-            records.append(Record(label, sentences))
+            record = Record(label, title, text)
+            records.append(record)
 
     return records
 
 
-def lemmatize_and_stem(records: [Record]):
+def tokenize_records(records: [Record]):
+    tokenizer = Tokenizer()
+    tokenized_records = []
+    for record in records:
+        tokenized_record = record.tokenize(tokenizer)
+        tokenized_records.append(tokenized_record)
+
+    return tokenized_records
+
+
+def lemmatize_and_stem(records: [TokenizedRecord]):
     lemmatizer = WordNetLemmatizer()
     stemmer = SnowballStemmer("english")
     for record in records:
@@ -50,7 +46,7 @@ def lemmatize_and_stem(records: [Record]):
         record.stem(stemmer)
 
 
-def write_records(path: str, records: [Record]):
+def write_records(path: str, records: [TokenizedRecord]):
     last_indexes = {}
     for record in records:
         label: str = record.label
@@ -64,19 +60,18 @@ def write_records(path: str, records: [Record]):
         with open(f"{folder}/{last_index}.tsv", 'w', newline='') as file:
             writer = csv.writer(file, delimiter='\t', dialect='excel-tab')
 
-            for sentence in record.sentences:
-                for token in sentence.tokens:
+            for sentence_tokens in record.sentences_tokens:
+                for token in sentence_tokens:
                     writer.writerow([token.text, token.lemma, token.stemma])
                 writer.writerow('')
 
 
-def create_dict(records: [Record]):
+def create_dict(records: [TokenizedRecord]):
     tokens = set()
     for record in records:
-        for sentence in record.sentences:
-            for token in sentence.tokens:
-                if token.text not in tokens:
-                    tokens.add(token.text)
+        for token in itertools.chain.from_iterable(record.sentences_tokens):
+            if token.text not in tokens:
+                tokens.add(token.text)
 
     with open(r"../../assets/dictionary.csv", 'w', newline='') as file:
         writer = csv.writer(file)
@@ -87,9 +82,10 @@ def main(in_path: str, out_path: str):
     init_ntlk()
 
     records = read_records(in_path)
-    lemmatize_and_stem(records)
-    create_dict(records)
-    write_records(out_path, records)
+    tokenized_records = tokenize_records(records)
+    lemmatize_and_stem(tokenized_records)
+    create_dict(tokenized_records)
+    write_records(out_path, tokenized_records)
 
 
 if __name__ == "__main__":
